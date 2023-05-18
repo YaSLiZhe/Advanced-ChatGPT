cc812915657be6fbb7be58e06993fb2db6094cfd
LiZhe <1756368629@qq.com>
2023-05-17 23:07:22 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>bug</li>
+        </ul>
+        <nav>
+          <p>Made by Zhe</p>
+        </nav>
+      </section>
+      <section className="main"></section>
     </div>
   );
-}
+};
 
 export default App;
