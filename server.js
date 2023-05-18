32f33f283c139034357cccc1e7ab5b3e896439c3
LiZhe <1756368629@qq.com>
2023-05-18 00:09:54 -0400

    Second commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const PORT = 8000;
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const API_KEY = process.env.API_KEY;
+
+const app = express();
+
+app.use(express.json);
+app.use(cors());
+
+app.post('/completions', async (req, res) => {
+  const options = {
+    method: 'post',
+    headers: {
+      'Authorization': `Bearer ${API_KEY}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      'model': 'gpt-3.5-turbo',
+      'messages': [{ 'role': 'user', 'content': 'how are you guys' }],
+      max_tokens: 1000,
+    }),
+  };
+  try {
+    const response = await fetch(
+      'https://api.openai.com/v1/chat/completions',
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
+app.listen(PORT, () => {
+  console.log('Server is running on port :' + PORT);
+});
--- src/App.js
+++ src/App.js
@@ -10,7 +10,19 @@ const App = () => {
           <p>Made by Zhe</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>ZheGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit" onClick={getMessages}>
+              Send
+            </div>
+          </div>
+          <p className="info">Chat GPT MAY 17</p>
+        </div>
+      </section>
     </div>
   );
 };
